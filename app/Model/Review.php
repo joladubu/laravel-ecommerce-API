14cf436d2d1edd120e575fc26f5e7594e6072fa9
joladubu <ajibola.oladubu@gmail.com>
2018-06-01 04:04:28 +0100

    Seeding completed for Product and Review

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Model;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Review extends Model
 {
-    //
+    protected $fillable=['customer','star','review'];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        //Using Faker Library to seed data into tables
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
